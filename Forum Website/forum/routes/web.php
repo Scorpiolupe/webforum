f7662c3d3888a3e95ac7098b57ab3d61cc0d98a7
yusufk8 <yusuffk888@gmail.com>
2025-03-05 18:23:16 +0300

    forum1

--- Forum Website/forum/routes/web.php
+++ Forum Website/forum/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+<<<<<<< Updated upstream
 use App\Http\Controllers\CategoryController;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
@@ -10,3 +11,12 @@ Route::get('/topics', [PageController::class, 'topics'])->name('topics');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 Route::get('/', [CategoryController::class, 'index'])->name('home');
 Route::get('/categories/{id}', [CategoryController::class, 'show']);
+=======
+use app\Http\Controllers\CategoryController;
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('categories',[CategoryController::class, 'index']);
+Route::get('categories/{id}',[CategoryController::class, 'show']);
+>>>>>>> Stashed changes
