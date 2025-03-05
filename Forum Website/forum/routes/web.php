e207d7d82f0b00c1dd4ed6f6a751ffb47b7ef0e2
yusufk8 <yusuffk888@gmail.com>
2025-03-05 18:22:58 +0300

    forum

--- Forum Website/forum/routes/web.php
+++ Forum Website/forum/routes/web.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
 Route::get('/categories', [PageController::class, 'categories'])->name('categories');
 Route::get('/topics', [PageController::class, 'topics'])->name('topics');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+Route::get('/', [CategoryController::class, 'index'])->name('home');
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
