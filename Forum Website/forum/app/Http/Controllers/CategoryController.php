ae47e36f66528253d9ceb9e421a796ab766c56d0
Ahmet Faruk Yaşar <ahmetfaruk6420@gmail.com>
2025-03-05 17:07:55 +0300

    category

--- Forum Website/forum/app/Http/Controllers/CategoryController.php
+++ Forum Website/forum/app/Http/Controllers/CategoryController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = Category::all();
+        return view('categories.index', compact('categories'));
+    }
+
+    public function show($id)
+    {
+        $category = Category::findOrFail($id);
+        return view('categories.show', compact('category'));
+    }
+
+    public function __construct()
+    {
+        view()->share('categories', Category::all());
+    }
 }
